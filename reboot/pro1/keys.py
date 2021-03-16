83414a9e40d9a678847a22d8287d22c9fc8a5d22
GauravShinde <53346638+gourav-shinde@users.noreply.github.com>
2021-03-16 17:16:05 +0530

    Update keys.py

--- reboot/pro1/keys.py
+++ reboot/pro1/keys.py
@@ -1,6 +1,6 @@
 EMAIL_USE_TLS=True
 EMAIL_HOST="smtp.gmail.com"
-EMAIL_HOST_USER="gauravshinde696969@gmail.com"
-EMAIL_HOST_PASSWORD="Gokugaurav@98"
+EMAIL_HOST_USER="email@gmail.com" #email here
+EMAIL_HOST_PASSWORD="Pass@98"#password here
 EMAIL_PORT=587
 SECRET_KEY = '+9g5^zq9clag68$_7vxli2cu@drw*f6(^hoj%^3y-p&wgp7!v^'
